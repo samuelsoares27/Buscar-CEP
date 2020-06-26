41fe6f21634fb06c94621d75783602cf7b322a48
Samuel Soares <34168199+samuelsoares27@users.noreply.github.com>
2020-06-26 11:14:00 -0300

    Initial commit

---
+++ Buscar CEP/Buscar CEP/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Buscar_CEP.Servicos;
+using Buscar_CEP.Servicos.Modelos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Buscar_CEP
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            try
+            {
+                if ((this.CEP.Text != "") && (this.CEP.Text != null))
+                {
+                    string cep = this.CEP.Text.Trim();
+                    Endereco end = EnderecoViaCEP.BuscarEnderecoViaCEP(cep);
+
+                    if (end != null)
+                    {
+                        DisplayAlert("Informação", "\nCEP:" + end.cep +
+                                                   "\nLogradouro: " + end.logradouro +
+                                                   "\nComplemento:" + end.complemento +
+                                                   "\nBairro:" + end.bairro +
+                                                   "\nLocalidade: " + end.localidade +  
+                                                   "\nUF:" + end.uf, "Ok");
+                    }
+                    else
+                    {
+                        DisplayAlert("Informação", "CEP não foi encontrado", "Ok");
+                    }
+                   
+                }
+                else
+                {
+                    DisplayAlert("Atenção", "Campo CEP está em branco", "Ok");
+                }
+
+            }
+            catch (Exception)
+            {
+
+                DisplayAlert("Atenção", "Erro ao buscar CEP", "Ok");
+            }
+        }
+    }
+}
---
+++ Buscar CEP/Buscar CEP/Servicos/EnderecoViaCEP.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Buscar_CEP.Servicos.Modelos;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace Buscar_CEP.Servicos
+{
+    class EnderecoViaCEP
+    {
+        private static string EnderecoURL = "http://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoCep = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string conteudo = wc.DownloadString(NovoEnderecoCep);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            if(end.cep == null)
+            {
+                return null;
+            }
+            else
+            {
+                return end;
+            }
+        }
+    }
+}
